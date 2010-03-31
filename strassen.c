ec2dc4fec419104fbbce822274c6058c5c11a282
Gabrielle Ehrlich <gehrlich@college.harvard.edu>
2010-03-31 00:51:55 -0400

    standard mult works

--- strassen.c
+++ strassen.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void standard(int dim,int a[dim][dim], int b[dim][dim], int c[dim][dim]);
+void strassen(int dim, int a[dim][dim],int b[dim][dim], int c[dim][dim]);
+void printmat(int dim, int a[dim][dim]);
+int rbyc(int dim, int a[dim][dim],int b[dim][dim], int i, int j);
+
 int main(int argc, char * argv[])
 {
     char * usage = "Usage: strassen 0 dimension inputfile";
@@ -37,36 +43,55 @@ int main(int argc, char * argv[])
             fscanf(inptr, "%d\n", &mat2[i][j]);
         }
     }
+    int test [dim][dim];
+    standard(dim,mat1, mat2,test);
+
+    printmat(dim,test);
+
     
     fclose(inptr);
 
 }
 
-
 void
-strassen(int a [][], int b [][], int c[][])
+strassen(int dim, int a [dim][dim], int b [dim][dim], int c[dim][dim])
 {
+    
 }
 
 void
-standard(int a[][],int b[][], int c [][], int dim)
+standard(int dim, int a[dim][dim],int b[dim][dim], int c [dim][dim])
 {
     for(int i = 0; i < dim; i++)
-    {
-        for(j = 0; j< dim; j++)
+    a{
+        for(int j = 0; j< dim; j++)
         {
-          c[i][j] = rbyc(a,b,i,j); 
+          c[i][j] = rbyc(dim,a,b,i,j); 
         }
     }
 }
 
 int
-rbyc(int a[][],int b[][], int i, int j, int dim)
+rbyc(int dim, int a[dim][dim],int b[dim][dim], int i, int j)
 {
     int sum = 0;
     for(int k= 0; k < dim; k++)
     {
         sum +=a[i][k] *b[k][j]; 
     }
-    return sum;:
+    return sum;
+}
+
+void 
+printmat(int dim, int a [dim][dim])
+{
+    for(int i = 0; i < dim; i++)
+    {
+        for(int j = 0; j< dim; j++)
+        {
+          printf("%d ",a[i][j]); 
+        }
+        printf("\n");
+    }
+
 }
