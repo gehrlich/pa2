ebc61b88f8d3e4102bfa51b4e0f76b7aef12f790
Gabrielle Ehrlich <gehrlich@college.harvard.edu>
2010-03-31 01:41:52 -0400

    strassen is really ugly :(

--- strassen.c
+++ strassen.c
@@ -3,9 +3,13 @@
 
 
 void standard(int dim,int a[dim][dim], int b[dim][dim], int c[dim][dim]);
-void strassen(int dim, int a[dim][dim],int b[dim][dim], int c[dim][dim]);
+void strassen(int dim, int a[dim][dim],int b[dim][dim], int c[dim][dim], int cross);
 void printmat(int dim, int a[dim][dim]);
 int rbyc(int dim, int a[dim][dim],int b[dim][dim], int i, int j);
+void matadd(int dim, int a [dim][dim], int b[dim][dim], int c[dim][dim], int x);
+void copypart(int dim1, int dim2, int x, int y, int a[dim1][dim1], int b[dim2][dim2]);
+
+
 
 int main(int argc, char * argv[])
 {
@@ -54,16 +58,55 @@ int main(int argc, char * argv[])
 }
 
 void
-strassen(int dim, int a [dim][dim], int b [dim][dim], int c[dim][dim])
+strassen(int dim, int x [dim][dim], int y [dim][dim], int z[dim][dim], int cross)
 {
+    if(dim < cross)
+        standard(dim,x,y,z);
+    
+    //idea optimize by not allocating any memeory here.  just pass in a start
+    //val and an end val and do additions in place
+    
+    //even only version:
+    //lots of memory allocation CUT DOWN!
+    int a [dim/2][dim/2]; 
+    int b [dim/2][dim/2]; 
+    int c [dim/2][dim/2];
+    int d [dim/2][dim/2];
+
+    int e [dim/2][dim/2];
+    int f [dim/2][dim/2];
+    int g [dim/2][dim/2];
+    int h [dim/2][dim/2];
+
+    copypart(dim, dim/2, 0, 0,  x, a);
+    copypart(dim, dim/2, 0, dim/2,  x, b);
+    copypart(dim, dim/2, dim/2, 0,  x, c);
+    copypart(dim, dim/2, dim/2, dim/2,  x, d);
+    copypart(dim, dim/2, 0, 0,  y, e);
+    copypart(dim, dim/2, 0, dim/2,  y, f);
+    copypart(dim, dim/2, dim/2, 0,  y, g);
+    copypart(dim, dim/2, dim/2, dim/2,  y, h);
+
+   // int p1 [dim/2][dim/2] = strassen(dim/2 ,a,matadd(1,f,h),z,cross); 
     
 }
 
 void
+copypart(int dim1, int dim2, int x, int y, int a [dim1][dim1], int b [dim2][dim2])
+{
+    for(int i = 0; i < dim2; i++)
+    {
+        for(int j = 0; j < dim2; j++)
+        {
+            b[i][j] = a [i+x][j+y];
+        }
+    }
+}
+void
 standard(int dim, int a[dim][dim],int b[dim][dim], int c [dim][dim])
 {
     for(int i = 0; i < dim; i++)
-    a{
+    {
         for(int j = 0; j< dim; j++)
         {
           c[i][j] = rbyc(dim,a,b,i,j); 
@@ -95,3 +138,20 @@ printmat(int dim, int a [dim][dim])
     }
 
 }
+
+void
+matadd(int dim, int a [dim][dim], int b[dim][dim], int c[dim][dim], int x)
+{
+    for(int i = 0; i < dim; i++)
+    {
+        for(int j = 0; j< dim; j++)
+        {
+            if(x==0)
+                c[i][j] = a[i][j] + b[i][j];
+            else
+                c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+
+}
+
